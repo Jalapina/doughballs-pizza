b9178c065d0d6c1be442d3d1bd4257f5eeb44d31
Jalapina <davidpina14@gmail.com>
2019-04-11 19:05:04 -0500

    Fixed header not aligning and added pizza animation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component'
+import { MenuComponent } from './menu/menu.component'
+import { AboutUsComponent  } from './about-us/about-us.component'
+import { GallaryComponent } from './gallary/gallary.component'
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: HomeComponent,
+  },
+  {
+    path: 'menu',
+    pathMatch: 'full',
+    component: MenuComponent,
+  },
+  {
+    path: 'about',
+    pathMatch: 'full',
+    component: AboutUsComponent,
+  },
+  {
+    path: 'gallary',
+    pathMatch: 'full',
+    component: GallaryComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { GallaryComponent } from './gallary/gallary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent,
+    AboutUsComponent,
+    GallaryComponent
   ],
   imports: [
     BrowserModule,
